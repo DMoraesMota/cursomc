64f238bb5c189c4d84acf678cb6a1aa24ef63658
diegomota@outlook.com <diegomota@outlook.com>
2019-04-14 13:08:37 -0300

    Estado e Cidade

---
+++ src/main/java/com/diegomota/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,14 @@
+package com.diegomota.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.diegomota.cursomc.domain.Estado;
+
+//O pacote repositories é responsável por acessar o banco de dados, é comparada a camada DAO, é a mesma coisa.
+//MESMA COISA QUE A CAMADA DAO
+// CAMADA DE ACESSO AOS DADOS
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+}
